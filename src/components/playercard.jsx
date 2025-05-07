bda6cd668de38b06228eb99a7f040df30c0e5ff1
jantokoci <153398095+jantokoci@users.noreply.github.com>
2025-05-07 11:02:13 +0200

    playercard a littlebit better

--- src/components/playercard.jsx
+++ src/components/playercard.jsx
@@ -1,12 +1,19 @@
-
+import "../styles/playercard.css"
 
 function PlayerCard({player}){
 
 
     return (
         <>
-            <div>
-                {player.id}
+            <div className="card">
+                <div>
+                    <p className="card-title">{player.playerName}</p>
+                    <p className={"card-text"}>age: {player.age}</p>
+                </div>
+                <div>
+                    <p className={"card-text"}>team: {player.team}</p>
+                    <p className={"card-text"}>games in this season: {player.games}</p>
+                </div>
             </div>
         </>
     )
