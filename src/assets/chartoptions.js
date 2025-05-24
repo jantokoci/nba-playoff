d31b2834766855b10cc493fb57b552dcc11028d1
jantokoci <153398095+jantokoci@users.noreply.github.com>
2025-05-25 00:19:27 +0200

    player data almmost fully done, maybe charts or no idk

---
+++ src/assets/chartoptions.js
@@ -0,0 +1,57 @@
+var options = {
+    series: [76],
+    chart: {
+        type: 'radialBar',
+        offsetY: -20,
+        sparkline: {
+            enabled: true
+        }
+    },
+    plotOptions: {
+        radialBar: {
+            startAngle: -90,
+            endAngle: 90,
+            track: {
+                background: "#e7e7e7",
+                strokeWidth: '97%',
+                margin: 5, // margin is in pixels
+                dropShadow: {
+                    enabled: true,
+                    top: 2,
+                    left: 0,
+                    color: '#444',
+                    opacity: 1,
+                    blur: 2
+                }
+            },
+            dataLabels: {
+                name: {
+                    show: false
+                },
+                value: {
+                    offsetY: -2,
+                    fontSize: '22px'
+                }
+            }
+        }
+    },
+    grid: {
+        padding: {
+            top: -10
+        }
+    },
+    fill: {
+        type: 'gradient',
+        gradient: {
+            shade: 'light',
+            shadeIntensity: 0.4,
+            inverseColors: false,
+            opacityFrom: 1,
+            opacityTo: 1,
+            stops: [0, 50, 53, 91]
+        },
+    },
+    labels: ['Average Results'],
+};
+
+export default options;
--- src/pages/playerpage.jsx
+++ src/pages/playerpage.jsx
@@ -3,6 +3,8 @@ import playerImage from "../assets/nba_players.js";
 import logoData from "../assets/logo-data.js";
 import "../styles/playerpage.css";
 
+
+
 export default function PlayerPage() {
 
     const location = useLocation();
@@ -24,8 +26,8 @@ export default function PlayerPage() {
     const age = player.age;
     const games = player.games;
     const minutesTotal = player.minutesPg;
-    const two = player.twoAttempts;
-    const twoA = player.twoAt;
+    const two = player.twoFg;
+    const twoA = player.twoAttempts;
     const three = player.threeFg;
     const threeA = player.threeAttempts;
     const ft = player.ft;
@@ -36,6 +38,20 @@ export default function PlayerPage() {
     const steals = player.steals
     const blocks = player.blocks
     const team = player.team;
+
+    const gameminutes = games*48;
+
+
+    const rpg = (offRb+defRb)/games;
+    const apg = assist/games;
+    const stpg = steals/games;
+    const bpg = blocks/games;
+
+    const points = two+three;
+    const ppg = points/games;
+    const att = twoA+threeA;
+
+
 
     return(
         <>
@@ -52,7 +68,76 @@ export default function PlayerPage() {
                         <p>Age: {age}</p>
                         <p>Games: {games}</p>
                     </div>
-                </div>
+                    </div>
+                    <div className="player-page-stats">
+                        <h2>Player Statistics</h2>
+                        <table className="stats-table">
+                            <thead>
+                            <tr>
+                                <th>Categories</th>
+                                <th>Season Total</th>
+                                <th>Average Per Game</th>
+                                <th>Percentage</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>Points</td>
+                                <td>{points}</td>
+                                <td>{ppg.toFixed(2)} PPG</td>
+                                <td>{((points/att).toFixed(2))*100}%</td>
+                            </tr>
+                            <tr>
+                                <td>Two Pointers</td>
+                                <td>{two}</td>
+                                <td>-</td>
+                                <td>{100*((two/twoA).toFixed(2))}%</td>
+                            </tr>
+                            <tr>
+                                <td>Three Pointers</td>
+                                <td>{three}</td>
+                                <td>-</td>
+                                <td>{100*((three/threeA).toFixed(2))}%</td>
+                            </tr>
+                            <tr>
+                                <td>Free Throws</td>
+                                <td>{ft}</td>
+                                <td>-</td>
+                                <td>{100*((ft/ftA).toFixed(2))}%</td>
+                            </tr>
+                            <tr>
+                                <td>Rebounds</td>
+                                <td>{offRb+defRb}</td>
+                                <td>{rpg.toFixed(2)} RPG</td>
+                                <td>-</td>
+                            </tr>
+                            <tr>
+                                <td>Assists</td>
+                                <td>{assist}</td>
+                                <td>{apg.toFixed(2)} APG</td>
+                                <td>-</td>
+                            </tr>
+                            <tr>
+                                <td>Setals</td>
+                                <td>{steals}</td>
+                                <td>{stpg.toFixed(2)} SPG</td>
+                                <td>-</td>
+                            </tr>
+                            <tr>
+                                <td>Blocks</td>
+                                <td>{blocks}</td>
+                                <td>{bpg.toFixed(2)} BPG</td>
+                                <td>-</td>
+                            </tr>
+                            <tr>
+                                <td>Minutes</td>
+                                <td>{minutesTotal}</td>
+                                <td>{(gameminutes/minutesTotal).toFixed(2)} MINS</td>
+                                <td>-</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
             </div>
         </>
     );
